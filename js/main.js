a7ac30a5579c887cb616d797e5dc56a89a9a3ebd
maarten079 <m.moerland.92@gmail.com>
2019-05-06 14:44:12 +0200

    added sections

--- js/main.js
+++ js/main.js
@@ -12,3 +12,28 @@ for (var i = 0; i < btns.length; i++) {
     this.className += " active";
   });
 }
+
+// Smooth Scrolling
+$('#navbar a').on('click', function(event) {
+  if (this.hash !== '') {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $('html, body').animate(
+      {
+        scrollTop: $(hash).offset().top - 100
+      },
+      800
+    );
+  }
+});
+
+// Sticky menu background
+window.addEventListener('scroll', function() {
+  if (window.scrollY > 100) {
+    document.querySelector('#navbar').style.opacity = 0.9;
+  } else {
+    document.querySelector('#navbar').style.opacity = 1;
+  }
+});
