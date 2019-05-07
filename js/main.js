8c596b135d35f7231124ab1c7e569fa65f497609
maarten079 <m.moerland.92@gmail.com>
2019-05-07 11:08:35 +0200

    refactored css

--- js/main.js
+++ js/main.js
@@ -7,9 +7,9 @@ var btns = btnContainer.getElementsByClassName("nav-item");
 // Loop through the buttons and add the active class to the current/clicked button
 for (var i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", function() {
-    var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
+    var current = document.getElementsByClassName("active-btn");
+    current[0].className = current[0].className.replace(" active-btn", "");
+    this.className += " active-btn";
   });
 }
 
